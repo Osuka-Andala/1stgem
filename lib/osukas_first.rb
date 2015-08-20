9538d51e57da50c69c76d509dc57912946ff3651
Osuka-Andala <teresa.andala@gmail.com>
2015-08-20 19:19:30 +0300

    added for loop

--- lib/osukas_first.rb
+++ lib/osukas_first.rb
@@ -2,8 +2,15 @@ require "osukas_first/version"
 
 module OsukasFirst
   # Your code goes here...
-  def self.add (a, b)
-  	a + b
-  end
+  def breaker
+  	for x in 0..5
+
+	if x > 3 then
+		break
+	end
+
+	puts "Value of local variable is #{x}"
+
+end
 
 end
